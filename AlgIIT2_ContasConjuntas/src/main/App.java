523dd0c74e1459626192e64f7eb781328ae949f3
IsraelDeorce <israel.deorce@acad.pucrs.br>
2017-10-16 15:54:02 -0200

    E Vamos desenrrolando...

--- AlgIIT2_ContasConjuntas/src/main/App.java
+++ AlgIIT2_ContasConjuntas/src/main/App.java
@@ -5,47 +5,64 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
+/**
+ * http://www.vogella.com/tutorials/JavaAlgorithmsDijkstra/article.html
+ * @author Israel
+ *
+ */
 public class App {
 
-	public static String tam="a";
+	public static int tam;
+	public static String nodoInicial, nodoFinal;
 	public static Grafo g = new Grafo();
 
 	public static void main(String[] args) {
 		// Grafo g = new Grafo();
-		// g.addVertice("A", Arrays.asList(new Vertice("B", 7), new Vertice("C", 8)));
-		// g.addVertice("B", Arrays.asList(new Vertice("A", 7), new Vertice("F", 2)));
-		// g.addVertice("C", Arrays.asList(new Vertice("A", 8), new Vertice("F", 6), new
+		// g.addVertice("A", Arrays.asList(new Vertice("B", 7), new Vertice("C",
+		// 8)));
+		// g.addVertice("B", Arrays.asList(new Vertice("A", 7), new Vertice("F",
+		// 2)));
+		// g.addVertice("C", Arrays.asList(new Vertice("A", 8), new Vertice("F",
+		// 6), new
 		// Vertice("G", 4)));
 		// g.addVertice("D", Arrays.asList(new Vertice("F", 8)));
 		// g.addVertice("E", Arrays.asList(new Vertice("H", 1)));
-		// g.addVertice("F", Arrays.asList(new Vertice("B", 2), new Vertice("C", 6), new
+		// g.addVertice("F", Arrays.asList(new Vertice("B", 2), new Vertice("C",
+		// 6), new
 		// Vertice("D", 8), new Vertice("G", 9), new Vertice("H", 3)));
-		// g.addVertice("G", Arrays.asList(new Vertice("C", 4), new Vertice("F", 9)));
-		// g.addVertice("H", Arrays.asList(new Vertice("E", 1), new Vertice("F", 3)));
+		// g.addVertice("G", Arrays.asList(new Vertice("C", 4), new Vertice("F",
+		// 9)));
+		// g.addVertice("H", Arrays.asList(new Vertice("E", 1), new Vertice("F",
+		// 3)));
 		// System.out.println(g.getShortestPath("A", "H"));
 
-		load("tinyG.txt");
-		System.out.println("a?");
+		load("enunciadoTeste.txt");
+		System.out.println("finalizou");
 	}
 
 	public static void load(String arquivo) {
 		Path path = Paths.get(arquivo);
-		try (Scanner sc = new Scanner(Files.newBufferedReader(path, Charset.forName("utf8")))) {
-			//sc.useDelimiter("[\\S*\n]");			
-			tam = sc.next();
+		try (Scanner sc = new Scanner(Files.newBufferedReader(path, Charset.forName("ISO-8859-1")))) {
+			tam = Integer.parseInt(sc.next());
 			System.out.println(tam);
-			String nConta, p1 = "a", p2 = "a";
-			while (sc.hasNext()) {
+			String nConta = "", p1 = "", p2 = "";
+			int cont = tam;
+			while (cont>0) {
 				nConta = sc.next();
-				if (sc.hasNext())
 				p1 = sc.next();
-				if (sc.hasNext())
-					p2 = sc.next();
-				System.out.println(nConta + " " + p1 + " " + p2);
+				p2 = sc.next();
+				System.out.println(nConta + " " + p1 + " " + p2); //PRINT
+				cont--;
+				g.addVertice("p1", Arrays.asList(new Vertice("B", 1), new Vertice("C", 1)));
 			}
+			nodoInicial = sc.next();
+			nodoFinal = sc.next();
+			System.out.println(nodoInicial + " " + nodoFinal); //PRINT
 		} catch (IOException e) {
 			System.out.println("FALHOU");
 			e.printStackTrace();
@@ -54,6 +71,13 @@ public class App {
 			e1.printStackTrace();
 		}
 
+	}
+	public static void adicionaAresta(String p1, String p2){
+		if(g.getArestas(p1)==null){
+			List<Vertice> listaV = new ArrayList<>();
+			listaV.add(new Vertice(p1,1));
+			g.addVertice(p1, listaV);
+		}
 	}
 
 }
--- AlgIIT2_ContasConjuntas/src/main/Grafo.java
+++ AlgIIT2_ContasConjuntas/src/main/Grafo.java
@@ -7,23 +7,36 @@ import java.util.Map;
 import java.util.PriorityQueue;
 
 public class Grafo {
-	
+
 	private final Map<String, List<Vertice>> vertices;
-	
+
 	public Grafo() {
 		this.vertices = new HashMap<String, List<Vertice>>();
 	}
-	
+
 	public void addVertice(String String, List<Vertice> Vertice) {
 		this.vertices.put(String, Vertice);
 	}
-	
+
+	/**
+	 * Retorna a lista de vértices que um determinado nodo se conecta,
+	 * ou Nulo se não se conectar com ninguém
+	 * @param vertice
+	 * @return
+	 */
+	public List<Vertice> getArestas(String vertice) {
+		if (!vertices.get(vertice).isEmpty())
+			return vertices.get(vertice);
+		else
+			return null;
+	}
+
 	public List<String> getShortestPath(String start, String finish) {
 		final Map<String, Integer> distances = new HashMap<String, Integer>();
 		final Map<String, Vertice> previous = new HashMap<String, Vertice>();
 		PriorityQueue<Vertice> nodes = new PriorityQueue<Vertice>();
-		
-		for(String Vertice : vertices.keySet()) {
+
+		for (String Vertice : vertices.keySet()) {
 			if (Vertice == start) {
 				distances.put(Vertice, 0);
 				nodes.add(new Vertice(Vertice, 0));
@@ -33,7 +46,7 @@ public class Grafo {
 			}
 			previous.put(Vertice, null);
 		}
-		
+
 		while (!nodes.isEmpty()) {
 			Vertice smallest = nodes.poll();
 			if (smallest.getId() == finish) {
@@ -48,15 +61,14 @@ public class Grafo {
 			if (distances.get(smallest.getId()) == Integer.MAX_VALUE) {
 				break;
 			}
-						
+
 			for (Vertice neighbor : vertices.get(smallest.getId())) {
 				Integer alt = distances.get(smallest.getId()) + neighbor.getDistance();
 				if (alt < distances.get(neighbor.getId())) {
 					distances.put(neighbor.getId(), alt);
 					previous.put(neighbor.getId(), smallest);
-					
-					forloop:
-					for(Vertice n : nodes) {
+
+					forloop: for (Vertice n : nodes) {
 						if (n.getId() == neighbor.getId()) {
 							nodes.remove(n);
 							n.setDistance(alt);
@@ -67,8 +79,8 @@ public class Grafo {
 				}
 			}
 		}
-		
+
 		return new ArrayList<String>(distances.keySet());
 	}
-	
+
 }
