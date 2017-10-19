0f76fc78f0de866328128faf0951bc72d10027fa
hercilio <hercilio@hercilio>
2017-10-19 17:34:00 -0200

    Versão Jaguar inserida

---
+++ AlgIIT2_ContasConjuntas/src/Jaguar/Vertice.java
@@ -0,0 +1,19 @@
+package Jaguar;
+
+public class Vertice {
+	public String nome;
+
+	public Vertice(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	@Override
+	public String toString() {
+		return nome;
+	}
+	
+}
